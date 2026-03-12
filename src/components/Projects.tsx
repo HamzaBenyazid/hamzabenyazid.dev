9e6d2c7b251c95a13a82150943685b77523cfaf0
Hamza Ben Yazid <benyazidhamza969@gmail.com>
2026-03-12 05:24:27 +0000

    Update image class to use full width and height

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -41,7 +41,7 @@ export default function Projects() {
                           alt={project.title}
                           width={40}
                           height={40}
-                          className="object-contain w-3/4 h-3/4"
+                          className="object-contain w-full h-full"
                         />
                       ) : (
                         <HiFolderOpen className="w-5 h-5" />
